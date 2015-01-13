d561a0cdec7d11053edcb9271c38d3f9b7b4ade2
Anssi Kääriäinen <akaariai@gmail.com>
2015-01-13 13:58:46 +0200

    Removed non-necessary comment

--- django_dockerfile/generate_dockerfile.py
+++ django_dockerfile/generate_dockerfile.py
@@ -8,8 +8,6 @@ import json
 import codecs
 
 """Backport of importlib.import_module from 3.x."""
-# While not critical (and in no way guaranteed!), it would be nice to keep this
-# code compatible with Python 2.3.
 
 def _resolve_name(name, package, level):
     """Return the absolute name of the module to be imported."""
